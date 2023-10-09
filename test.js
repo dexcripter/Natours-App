e07ccd9b8bca1645e0d112d8900751b7667f4a10
Johnpaul <indisputable.jay@gmail.com>
2023-10-09 19:29:44 +0100

    Daily

--- test.js
+++
@@ -1,8 +0,0 @@
-function truncateString(str, num) {
-  let check = str.split(" ");
-  if (str.length > num) {
-    console.log(str);
-  } else return str;
-}
-
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
