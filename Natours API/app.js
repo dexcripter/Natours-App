0ca43fd990245c3943ebd35abf1c28cd8aceabc0
Johnpaul <indisputable.jay@gmail.com>
2023-10-10 23:31:54 +0100

    Learnt params in routing

--- Natours API/app.js
+++ Natours API/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
-app.use(express());
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`/${__dirname}/dev-data/data/tours-simple.json`)
@@ -18,12 +18,37 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
-app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body);
-  res.send("Working");
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (id > tours.length - 1) {
+    return res.status(404).json({
+      status: "error",
+      message: "Tour not found",
+    });
+  }
+  res.status(200).json({ status: "success!", data: tour });
 });
 
-const port = 3000;
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1] + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `/${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
