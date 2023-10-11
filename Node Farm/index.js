283886358a9657a9691d946df730ce8e610a5b3f
Johnpaul <indisputable.jay@gmail.com>
2023-10-12 00:47:55 +0100

    Slight progress

--- Node Farm/index.js
+++ Node Farm/index.js
@@ -5,7 +5,9 @@ const url = require('url');
 const app = express();
 const port = 3000;
 
-//  READING  TEMPLATE FILES & LOCAL API
+app.use(express.json());
+
+///  READING  TEMPLATE FILES & LOCAL API
 const tempCard = fs.readFileSync(
     `${__dirname}/templates/template-card.html`,
     'utf-8'
@@ -18,18 +20,43 @@ const tempProduct = fs.readFileSync(
     `${__dirname}/templates/template-product.html`,
     'utf-8'
 );
-const data = JSON.parse(
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 );
+
+/// FUNCTIONS
+function replaceTemplates(temp, product) {
+    let output = temp;
+    output = output.replace(/{%NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%LOCATION%}/g, product.from);
+    output = output.replace(/{%VITAMINS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%%}/g, product.id);
+    return output;
+}
 
 // ROUTING
 app.get('/', (req, res) => {
     console.log(data.length);
+    fs.writeFileSync(
+        `${__dirname}/templates/template-card.html`,
+        JSON.stringify(data)
+    );
     res.send(tempOverview);
 });
-
-app.get('/product' || 'Okay', (req, res) => {
-    res.send(tempProduct);
+app.get('/overview', (req, res) => {
+    const cardsHtml = dataObj
+        .map((el) => replaceTemplates(tempCard, el))
+        .join();
+    const output = tempOverview.replace(/{%TEMPLATE-OVERVEIW%}/, cardsHtml);
+    res.end(output);
+});
+app.get('/product/:id', (req, res) => {
+    res.send(req.params.id);
 });
 
 // LISTENING TO SERVER
