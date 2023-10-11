3d2cd7500b1301fc7e9a34b0635884e41a49fe5b
Johnpaul N <133887442+mrJayPEG@users.noreply.github.com>
2023-10-11 22:10:21 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const writeFile = fs.readFileSync("./txt/append.txt", "utf-8");
-const reading = fs.readFileSync("./txt/start.txt", "utf-8");
-const readingg = fs.readFileSync(`./txt/${reading}.txt`, "utf-8");
-
-// console.log(writeFile);
-console.log(readingg);
