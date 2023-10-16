2f19cebd4b60e917738a6cd443a34b40a90444ff
Johnpaul <indisputable.jay@gmail.com>
2023-10-16 15:40:54 +0100

    Replacing

--- Natours API/server.js
+++ Natours API/server.js
@@ -1,10 +1,9 @@
-const app = require('./app');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// SERVER
-const port = 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`App running on port ${port}...`);
 });
