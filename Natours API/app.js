28a7dcaf08aa9d8852237734ee11a863c8d6259e
Johnpaul <indisputable.jay@gmail.com>
2023-10-18 00:23:14 +0100

    Added some new devDepdencies

--- Natours API/app.js
+++ Natours API/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-console.log(process.env.PORT);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
