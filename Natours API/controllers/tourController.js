b2d18b5d28ddb725403b6269c720c26e93f0ef68
Johnpaul <indisputable.jay@gmail.com>
2023-10-25 09:29:14 +0100

    pause

--- Natours API/controllers/tourController.js
+++ Natours API/controllers/tourController.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-useless-path-segments
 const Tour = require('./../models/tourModel');
-const Tours = require('./../models/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
@@ -11,7 +10,6 @@ exports.getAllTours = async (req, res) => {
     const tours = await Tour.find();
     res.status(200).json({
       status: 'success',
-      requestedAt: req.requestTime,
       results: tours.length,
       data: {
         tours,
@@ -22,18 +20,19 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  console.log(process.env);
-  const id = req.params.id * 1;
-
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // findById is a query in mongosh. // Tour.findOne({_id: req.params.id})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 exports.createTour = async (req, res) => {
