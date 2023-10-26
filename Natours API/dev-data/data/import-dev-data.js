be625a76777a42e2b68053db89bb6f72b9ce2c5b
Johnpaul <indisputable.jay@gmail.com>
2023-10-26 16:46:21 +0100

    First time rinning scripts

--- Natours API/dev-data/data/import-dev-data.js
+++ Natours API/dev-data/data/import-dev-data.js
@@ -1,6 +1,9 @@
-const dotenv = require('dotenv');
 const fs = require('fs');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+const Tour = require('./../../models/tourModel');
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
@@ -15,3 +18,37 @@ mongoose
   .then(() => {
     console.log('DB connectin was successful!');
   });
+
+// READING JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Succesfully loaded!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATAA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data Succesfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
