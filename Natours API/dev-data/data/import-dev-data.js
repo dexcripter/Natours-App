262ebe3e72f99878c84d0d8e5d5e5388bf579c81
Johnpaul <indisputable.jay@gmail.com>
2023-10-26 16:21:14 +0100

    Completed CRUD

---
+++ Natours API/dev-data/data/import-dev-data.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connectin was successful!');
+  });
--- Natours API/models/tourModel.js
+++ Natours API/models/tourModel.js
@@ -4,17 +4,54 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name!'],
+
+    trim: true,
     unique: true,
   },
-  ratings: {
+  duration: {
     type: Number,
-    required: true,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
     default: 4.0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'a tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover Image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
