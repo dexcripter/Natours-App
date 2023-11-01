6c798c1b4517ecce43cf5943c49db5755f60979f
Johnpaul <indisputable.jay@gmail.com>
2023-11-01 19:56:18 +0100

    Refactored

--- Natours API/app.js
+++ Natours API/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -17,10 +19,21 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'falied',
-    message: `There is no ${req.originalUrl} defined on this server!`,
-  });
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'falied',
+  //   message: `There is no ${req.originalUrl} defined on this server!`,
+  // });
+
+  // const err = new Error(
+  //   `There is no ${req.originalUrl} defined on this server!`,
+  // );
+  // err.status = 'failed';
+  // err.statusCode = 404;
+
+  next(new AppError(`There is no ${req.originalUrl} defined on this server!`));
 });
+
+app.use(globalErrorHandler);
+
 module.exports = app;
