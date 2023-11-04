4e74dbd5c9ac967b71696c841501d37f654cb634
Johnpaul <indisputable.jay@gmail.com>
2023-11-04 18:34:09 +0100

    Password managment

--- Natours API/models/userModules.js
+++ Natours API/models/userModules.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,8 +33,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
