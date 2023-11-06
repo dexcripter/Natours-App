3fdefbf713457a519290acd8fa27cb674b5573b6
Johnpaul <indisputable.jay@gmail.com>
2023-11-06 14:29:50 +0100

    Im having an error

--- Natours API/controllers/authController.js
+++ Natours API/controllers/authController.js
@@ -3,6 +3,7 @@ const { promisify } = require('util');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModules');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 // function signedToken(id) {
 //   jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -122,5 +123,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) send it to the user's email
+  const resetUrl = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a patch requeset with your ne wpasswrd and password conform to ${resetUrl}\nIf you didnt initiate this email, please igonore this.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 mins)',
+      message,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to mail',
+    });
+  } catch (err) {
+    (user.createPasswordResetToken = undefined),
+      (user.passwordResetExpires = undefined);
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email.Try again later', 500),
+    );
+  }
 });
 exports.resetPassword = (req, res, next) => {};
---
+++ Natours API/utils/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1) CREATE A TRANSPORTER
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    // Activate in gmail "less secure app" option
+  });
+
+  // 2) DEFINE THE EMAIL OPTIONS
+  const mailOptions = {
+    from: 'Johnpaul Nnaji <indisputable.jay@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.email,
+    // html
+  };
+  // 3) SENDING THE EMAIL WITH NODEMAILER
+  await transporter.semdMail({ mailOptions });
+};
+
+module.exports = sendEmail;
