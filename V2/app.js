8a0beaf1db501fe83cad5a2d14fbc7733689762d
Johnpaul <indisputable.jay@gmail.com>
2023-11-08 14:31:51 +0100

    Added user routes and about to better my file structure

--- V2/app.js
+++ V2/app.js
@@ -2,25 +2,39 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourController = require('../V2/contollers/tour-controller');
+const userController = require('./contollers/user-controller');
 
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(morgan('dev'));
-// const route = express.Router;
+const tourRouter = express.Router();
+const userRouter = express.Router();
+app.use('/api/v1/users', userRouter);
 
-app
-  .route('/api/v1/tours/:id')
+app.use('/api/v1/tours', tourRouter);
+tourRouter
+  .route('/:id')
   .all(tourController.checkId)
   .get(tourController.getTour)
   .delete(tourController.deleteTour)
   .patch(tourController.updateTour);
 
-app
-  .route('/api/v1/tours')
+tourRouter
+  .route('/')
   .get(tourController.getAllTour)
   .post(tourController.createTour);
+
+userRouter
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+userRouter
+  .route('/:id')
+  .delete(userController.deleteUser)
+  .get(userController.getUser)
+  .patch(userController.updateUser);
 
 const port = 3000;
 app.listen(port, () => {
