2095ab5db0a919f03f2cc3d12813deb0fec14462
Johnpaul <indisputable.jay@gmail.com>
2023-11-11 14:04:15 +0100

    mongo middlewares

--- V2/contollers/tour-controller.js
+++ V2/contollers/tour-controller.js
@@ -106,8 +106,12 @@ exports.updateTour = async (req, res) => {
 };
 
 exports.deleteTour = async (req, res) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-  res.status(204).json({ status: 'success', data: null });
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.getTourStats = async (req, res) => {
