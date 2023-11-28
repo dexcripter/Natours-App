b3811cae7e29bdf432b45a8d33ef4880b06492b6
Johnpaul <indisputable.jay@gmail.com>
2023-11-28 15:05:41 +0100

    implemented quering featers

--- Natours/controllers/tour-controller.js
+++ Natours/controllers/tour-controller.js
@@ -2,7 +2,53 @@ const Tour = require('../models/tour-schema');
 
 exports.getTours = async (req, res, next) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+
+    // FILTERING
+
+    // removing unwanted fields
+    const exculdedFields = ['page', 'sort', 'limit', 'fields'];
+    exculdedFields.forEach((el) => delete queryObj[el]);
+
+    // convert to string and use regex to match (gte gt lte le) mongoose operations
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let query = Tour.find(JSON.parse(queryString));
+
+    // SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // FIELDS
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // PAGINATE
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const newTours = await Tour.countDocuments();
+      if (skip > newTours) throw new Error('No such documents');
+    }
+
+    const tours = await query;
+    console.log(tours);
     res.status(200).json({
       status: 'success',
       results: tours.length,
