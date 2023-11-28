771b086f7fdc67530dddf80605b017e1af20606f
Johnpaul <indisputable.jay@gmail.com>
2023-11-28 07:48:37 +0100

    modelling the data

--- Natours/models/tour-schema.js
+++ Natours/models/tour-schema.js
@@ -6,16 +6,53 @@ const tourSchema = mongoose.Schema({
     required: [true, 'A tour requires a name!'],
     unique: [true],
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     min: 1,
     max: 5,
-    enum: [1, 2, 3, 4, 5],
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    enum: ['easy', 'medium', 'difficult'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
