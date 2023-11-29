d067cb22c1724b917b16198d802049a43ad535db
Johnpaul <indisputable.jay@gmail.com>
2023-11-29 13:02:42 +0100

    utilizing mongoose middleware

--- Natours/models/tour-schema.js
+++ Natours/models/tour-schema.js
@@ -1,60 +1,92 @@
+const { query } = require('express');
 const mongoose = require('mongoose');
+const slug = require('slug');
 
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour requires a name!'],
-    unique: [true],
+const tourSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour requires a name!'],
+      unique: [true],
+    },
+    slug: String,
+    ratingsAverage: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      enum: ['easy', 'medium', 'difficult'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  ratingsAverage: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    enum: ['easy', 'medium', 'difficult'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// virtual properties
+tourSchema.virtual('durationWeeks').get(function () {
+  return Math.floor(this.duration / 7);
 });
 
-const Tour = mongoose.model('Tour', tourSchema);
+// mongoose document middleware
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
+// query middleware
+tourSchema.pre(/^find/, function (docs, next) {
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`query took ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
+// aggregate middleware
+
+const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
--- Natours/routers/tour-routes.js
+++ Natours/routers/tour-routes.js
@@ -6,10 +6,12 @@ const {
   findTour,
   updateTour,
   deleteTour,
+  getTourStats,
 } = require('../controllers/tour-controller');
 
 const router = express.Router();
 
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getTours).post(createTour);
 router.route('/:id').get(findTour).patch(updateTour).delete(deleteTour);
 
