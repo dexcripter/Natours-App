4f87a8f98177bce2f85fb6c5b89401afa606ff6e
Johnpaul <indisputable.jay@gmail.com>
2023-12-01 11:41:25 +0100

    handling errors

--- Natours/app.js
+++ Natours/app.js
@@ -15,4 +15,12 @@ app.use(express.json());
 // routing
 app.use('/api/v1', apiV1);
 
+// uncaught routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
