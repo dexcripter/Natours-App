ed013e415464ce7ae72f6afbd241ef9ab70ef4ac
Johnpaul <indisputable.jay@gmail.com>
2023-12-05 22:11:25 +0100

    data protection and sanitization

--- Natours/app.js
+++ Natours/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const apiV1 = require('./api-versioning/apiV1');
@@ -9,10 +14,26 @@ const app = express();
 
 // middlewares
 // app.use(express.static('./public'));
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: ['duration'],
+  })
+);
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 3600 * 1000,
+  message: 'Too many requirest from this IP, please try again later',
+});
+app.use('/api', limiter);
 
 // routing
 app.use('/api/v1', apiV1);
