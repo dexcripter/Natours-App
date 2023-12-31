bca5dd2b5a24b297b51f993d604dfad90d9c9445
Johnpaul <indisputable.jay@gmail.com>
2023-12-31 21:43:46 +0100

    using cookies

--- Natours/V2/src/controllers/auth-controller.js
+++ Natours/V2/src/controllers/auth-controller.js
@@ -16,6 +16,21 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRATION * 24 * 60 * 60 * 1000
+    ),
+    secure: false,
+    httpOnly: true, // prevents corss-site scripting attacks
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
