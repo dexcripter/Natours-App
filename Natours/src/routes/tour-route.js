2ab96177022e2ba0d30d0b8bef3d92e13399944b
Johnpaul <indisputable.jay@gmail.com>
2024-01-07 21:40:29 +0100

    indexing

--- Natours/src/routes/tour-route.js
+++ Natours/src/routes/tour-route.js
@@ -7,9 +7,15 @@ const { protect, restrictTo } = require('../controllers/auth-controller');
 const Router = express.Router();
 
 Router.route('/tour-stats').get(controller.getTourStats);
-Router.route('/getmonthlyplan/:year').get(controller.getMonthlyPlan);
+Router.route('/getmonthlyplan/:year').get(
+  protect,
+  restrictTo('admin', 'lead-guide', 'guide'),
+  controller.getMonthlyPlan
+);
 
-Router.route('/').get(protect, controller.getTours).post(controller.createTour);
+Router.route('/')
+  .get(controller.getTours)
+  .post(protect, restrictTo('admin', 'lead-guide'), controller.createTour);
 Router.route('/:id')
   .get(controller.getTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), controller.deleteTour)
