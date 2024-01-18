cde91cf1a4b53d1a5639b441ed0ade1f0b110886
Johnpaul <indisputable.jay@gmail.com>
2024-01-18 18:19:08 +0100

    multer working

--- Natours/src/controllers/userController.js
+++ Natours/src/controllers/userController.js
@@ -1,7 +1,31 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+const multerStorge = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/dev-data/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/').at(1);
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Please upload only images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorge,
+  fileFilter: multerFilter,
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -10,6 +34,8 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
+
+exports.uploadUserPhoto = upload.single('photo'); // where the files would be uploaded to
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
