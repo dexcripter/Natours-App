33981e7f6035302daeb8ebd0797a93eb707638f2
Johnpaul <indisputable.jay@gmail.com>
2024-01-20 23:26:59 +0100

    formatting changes

--- Natours/src/controllers/userController.js
+++ Natours/src/controllers/userController.js
@@ -6,7 +6,7 @@ const factory = require('./handlerFactory');
 
 const multerStorge = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/dev-data/img/users');
+    cb(null, 'public/img/users');
   },
   filename: (req, file, cb) => {
     const ext = file.mimetype.split('/').at(1);
@@ -43,7 +43,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -56,6 +55,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
